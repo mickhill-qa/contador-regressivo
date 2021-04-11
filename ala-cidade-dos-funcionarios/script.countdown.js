29a60ddf44d3fec2b62f46cc2defc648f0c89762
Mick Hill <mickhill@hotmail.com.br>
2021-04-11 03:07:42 -0300

    Atualizacao 11/04/2021
    - Atualizando datas;
    - Atualizando linka;

--- ala-cidade-dos-funcionarios/script.countdown.js
+++ ala-cidade-dos-funcionarios/script.countdown.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	var note = $('#note'),
-		ts = new Date('2021/03/28 08:30:00'),
+		ts = new Date('2021/04/11 08:30:00'),
 		// ts = new Date('2021/02/27 13:40:00'),
 		newYear = true;
 	
